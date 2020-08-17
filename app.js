c5a4e1057f6ea10ab4a939a37418f127fd2edb67
firdausthedev <firdausthedev@gmail.com>
2020-08-17 20:37:46 +0800

    display lyrics

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const searchInput = document.getElementById('search-input')
 
 const songTitle = document.getElementById('song-title')
 
+const modal = document.getElementById('modal')
+const closeModal = document.getElementById('modal-close')
+const songTitleModal = document.querySelector('#modal-form h2')
+const songLyricModal = document.querySelector('#modal-form p')
+
 const APIURL = 'https://api.lyrics.ovh'
 let list = []
+let currentSongID
 
 searchBtn.addEventListener('click', (e) => {
+  list = []
   document.querySelectorAll('.song').forEach((song, index) => {
     song.remove()
   })
+
   getSongData(searchInput.value)
 })
 
@@ -29,11 +37,11 @@ const getSongData = async (name) => {
 }
 
 const getSongLyrics = (data) => {
+  list.push(data)
   const lyrics = data.lyrics.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
   const title = data.song.title
   const albumCover = data.song.album.cover_medium
   const artist = data.song.artist.name
-  console.log(data)
 
   const baseSong = document.createElement('div')
   baseSong.className = 'song'
@@ -49,6 +57,39 @@ const getSongLyrics = (data) => {
   <button class="get-lyric">Lyrics</button>
   
   `
-
   document.querySelector('#songs-details').appendChild(baseSong)
 }
+
+searchInput.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    getSongData(searchInput.value)
+  }
+})
+
+document.getElementById('songs-details').addEventListener('click', (e) => {
+  if (e.target.classList.contains('get-lyric')) {
+    const id = e.target.parentElement.id
+    displayLyrics(id)
+  }
+})
+
+// get lyrics by id
+const displayLyrics = (id) => {
+  list.forEach((li) => {
+    if (li.index == id) {
+      modal.style.display = 'block'
+      songTitleModal.innerHTML = li.song.title
+      songLyricModal.innerHTML = li.lyrics.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+    }
+  })
+}
+
+// close button inside modal
+closeModal.addEventListener('click', () => (modal.style.display = 'none'))
+
+//close modal if click outside of modal
+modal.addEventListener('click', (e) => {
+  if (e.target.classList.value === 'modal-backdrop') {
+    modal.style.display = 'none'
+  }
+})
